34c263631242ccfb6d9a8a2489dfff182c12d5e4
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-08-02 08:42:57 +0530

    nav : Added menus and configured bottom navigation.

--- app/src/main/java/com/dhruvlimbachiya/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/dhruvlimbachiya/runningapp/ui/MainActivity.kt
@@ -3,21 +3,35 @@ package com.dhruvlimbachiya.runningapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.dhruvlimbachiya.runningapp.R
 import com.dhruvlimbachiya.runningapp.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var mDao: RunDao
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Timber.i("onCreate: ${mDao.hashCode()}")
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment // Find the NavHostFragment.
+        bottomNavigationView.setupWithNavController(navHost.findNavController()) // Set navController to BottomNavigationView
+
+        navHost.findNavController().addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+                // Display BottomNavigationView only on these fragments.
+                R.id.runFragment,R.id.settingsFragment,R.id.statisticsFragment -> {
+                    bottomNavigationView.isVisible = true
+                }
+                else -> bottomNavigationView.isVisible = false
+            }
+        }
     }
 }
