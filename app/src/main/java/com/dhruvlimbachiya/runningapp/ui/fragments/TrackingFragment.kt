459f17afd197c0723b2fc6f266c33f0663bf2e5e
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-08-02 10:35:29 +0530

    Handle GMap lifecycles & loads the GMap.

--- app/src/main/java/com/dhruvlimbachiya/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/dhruvlimbachiya/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,14 @@
 package com.dhruvlimbachiya.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.dhruvlimbachiya.runningapp.R
 import com.dhruvlimbachiya.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 /**
  * Created by Dhruv Limbachiya on 30-07-2021.
@@ -14,4 +18,46 @@ import dagger.hilt.android.AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val mViewModel: MainViewModel by viewModels()
+
+    private var mGoogleMap: GoogleMap? = null
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView?.onCreate(savedInstanceState)
+
+        // Load the Google Map Asynchronously.
+        mapView.getMapAsync { map ->
+            mGoogleMap = map
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
 }
