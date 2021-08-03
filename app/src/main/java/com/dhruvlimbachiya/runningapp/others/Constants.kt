eed0922024e781440df4a0c26f2c6cedab5d0229
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-08-03 20:58:21 +0530

    serv : Track Runner Location.

--- app/src/main/java/com/dhruvlimbachiya/runningapp/others/Constants.kt
+++ app/src/main/java/com/dhruvlimbachiya/runningapp/others/Constants.kt
@@ -31,8 +31,4 @@ object Constants {
     const val POLYLINE_COLOR = Color.RED
     const val CAMERA_ZOOM = 15f
 
-
-    // Location Constants
-    const val LOCATION_UPDATE_INTERVAL = 5000L
-    const val LOCATION_UPDATE_FASTEST_INTERVAL = 2000L
 }
